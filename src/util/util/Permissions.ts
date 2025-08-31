4d4db50c4ab32b7d5ef5e96f054831e4204f4723
Erkin Alp Güney <erkinalp9035@gmail.com>
2025-08-31 18:50:32 +0300

    use a vacated permission bit for geospatial

--- src/util/util/Permissions.ts
+++ src/util/util/Permissions.ts
@@ -75,7 +75,7 @@ export class Permissions extends BitField {
 		USE_PUBLIC_THREADS: BitFlag(35),
 		USE_PRIVATE_THREADS: BitFlag(36),
 		USE_EXTERNAL_STICKERS: BitFlag(37),
-		VIEW_GEOSPATIAL: BitFlag(38),
+		VIEW_GEOSPATIAL: BitFlag(47),
 
 		/**
 		 * CUSTOM PERMISSIONS ideas:
